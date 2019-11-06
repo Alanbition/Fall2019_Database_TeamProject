83ab9044384264da8237c1251d05ea18d5dbffd0
Ruoyun Chen <chenruoyun115@gmail.com>
2019-11-06 17:23:16 -0500

    working on groupDao

---
+++ cs5200_fall2019_project/src/main/java/edu/northeastern/cs5200/daos/GroupDao.java
@@ -0,0 +1,15 @@
+package edu.northeastern.cs5200.daos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+import edu.northeastern.cs5200.repositories.GroupRepository;
+
+@Controller("GroupDao")
+@Service
+public class GroupDao {
+	@Autowired
+	GroupRepository groupRepository;
+
+}
