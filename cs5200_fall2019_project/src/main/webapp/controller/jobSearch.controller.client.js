9becbc2cdf1bcc57a5581419c1d93c32757e68a4
Ruoyun Chen <chenruoyun115@gmail.com>
2019-12-05 21:13:42 -0500

    get started with client

---
+++ cs5200_fall2019_project/src/main/webapp/controller/jobSearch.controller.client.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+(function () {
+    angular
+        .module("cs5200_fall2019_project")
+        .controller("jobController", jobController);
+    function jobController($keywords) {
+        
+    }
+})();
