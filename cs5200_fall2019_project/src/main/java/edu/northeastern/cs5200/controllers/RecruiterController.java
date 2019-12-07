d47ecf2ce16c6c9082e68edc157ff9a336b770d1
Ruoyun Chen <chenruoyun115@gmail.com>
2019-12-07 14:16:40 -0500

    complete recruiterDao

--- cs5200_fall2019_project/src/main/java/edu/northeastern/cs5200/controllers/RecruiterController.java
+++ cs5200_fall2019_project/src/main/java/edu/northeastern/cs5200/controllers/RecruiterController.java
@@ -1,5 +1,8 @@
 package edu.northeastern.cs5200.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.northeastern.cs5200.daos.RecruiterDao;
+import edu.northeastern.cs5200.models.Application;
+import edu.northeastern.cs5200.models.Job;
+import edu.northeastern.cs5200.models.JobInterest;
 import edu.northeastern.cs5200.models.Recruiter;
+import edu.northeastern.cs5200.models.Student;
+import edu.northeastern.cs5200.repositories.JobInterestRepository;
+import edu.northeastern.cs5200.repositories.JobRepository;
 import edu.northeastern.cs5200.repositories.RecruiterRepository;
 
 @RestController
@@ -18,6 +27,8 @@ public class RecruiterController {
 	@Autowired
 	RecruiterDao recruiterDao;
 	RecruiterRepository recruiterRepository;
+	JobRepository jobrepository;
+	JobInterestRepository jobInterestrepository;
 	
 	@GetMapping("api/recruiter")
 	public Iterable<Recruiter> findAllRecruiter() {
@@ -30,7 +41,7 @@ public class RecruiterController {
 		recruiterDao.deleteRecruiter();
     }
 	
-	@PutMapping("/api/recruiter/{eid}")
+	@PutMapping("/api/recruiter/{rid}")
 	public Recruiter updateRecruiter(@PathVariable("rid") int id, @RequestBody Recruiter r) {
 		Recruiter recruiter = recruiterDao.updateRecruiter(id, r);
 		return recruiter;
@@ -40,5 +51,40 @@ public class RecruiterController {
     public Recruiter createRecruiter(@RequestBody Recruiter recruiter) {
         return recruiterDao.createRecruiter(recruiter);
     }
+	
+	@PostMapping("/api/recruiter/{rid}/job/{jid}")
+    public Recruiter addJobToRecruiter(@PathVariable("rid") int rid,@PathVariable("jid") int jid) {
+		Job job = jobrepository.findById(jid).get();
+		Recruiter recruiter = recruiterRepository.findById(rid).get();
+        return recruiterDao.addJobToRecruiter(job,recruiter);
+    }
+	
+	@DeleteMapping("/api/recruiter/{rid}/job/{jid}")
+    public void deleteJobFromRecruiter(@PathVariable("rid") int rid,@PathVariable("jid") int jid) {
+		Job job = jobrepository.findById(jid).get();
+		Recruiter recruiter = recruiterRepository.findById(rid).get();
+		recruiterDao.removeJobfromRecruiter(job, recruiter);
+    }
+	
+
+	@GetMapping("api/recruiter/{rid}/job/application")
+	public List<Application> FindApplicationsByJobsPostedByRecruiters(@PathVariable("rid") int rid) {
+		Recruiter recruiter = recruiterRepository.findById(rid).get();
+		List<Job> jobs = recruiter.getJobsCreatedByRecruiter();
+		List<Application> applications = new ArrayList<Application>();
+		for(Job job:jobs) {
+			applications.addAll(job.getApplicationsForThisJob());
+		}
+		return applications;
+	}
+	
+	@GetMapping("api/recruiter/{rid}/job/{jid}/application")
+	public List<Application> FindApplicationsByJobsPostedByRecruiters(@PathVariable("rid") int rid,@PathVariable("jid") int jid) {
+		Job job = jobrepository.findById(jid).get();
+		List<Application> applications =job.getApplicationsForThisJob();
+		return applications;
+	}
+	
+
 
 }
